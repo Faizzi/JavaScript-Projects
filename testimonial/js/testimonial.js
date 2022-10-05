3dd7d524f4113fe787cbdc9bb5877c70f402de15
Faizan Ullah <faizanahmadkhan34@example.com>
2022-10-05 19:02:32 +0500

    Testimonial with Swiper js

--- testimonial/js/testimonial.js
+++ testimonial/js/testimonial.js
@@ -1,28 +1,10 @@
 
 
 // Infinite Loop Swiper Js
-// var swiper = new Swiper(".mySwiper", {
-//     slidesPerView: 1,
-//    grabCursur:true,
-//     loop: true,
-//     pagination: {
-//       el: ".swiper-pagination",
-//       clickable: true,
-//     },
-//     navigation: {
-//       nextEl: ".swiper-button-next",
-//       prevEl: ".swiper-button-prev",
-//     },
-//   });
-
-//   AutoPlay Swiper Start
 var swiper = new Swiper(".mySwiper", {
-    spaceBetween: 30,
-    centeredSlides: true,
-    autoplay: {
-      delay: 2500,
-      disableOnInteraction: false,
-    },
+    slidesPerView: 1,
+   grabCursur:true,
+    loop: true,
     pagination: {
       el: ".swiper-pagination",
       clickable: true,
@@ -32,6 +14,24 @@ var swiper = new Swiper(".mySwiper", {
       prevEl: ".swiper-button-prev",
     },
   });
+
+//   AutoPlay Swiper Start
+// var swiper = new Swiper(".mySwiper", {
+//     spaceBetween: 30,
+//     centeredSlides: true,
+//     autoplay: {
+//       delay: 2500,
+//       disableOnInteraction: false,
+//     },
+//     pagination: {
+//       el: ".swiper-pagination",
+//       clickable: true,
+//     },
+//     navigation: {
+//       nextEl: ".swiper-button-next",
+//       prevEl: ".swiper-button-prev",
+//     },
+//   });
 // Autoplay End
 
 // Effect CoverFlow
